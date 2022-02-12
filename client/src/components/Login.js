6fe6670ce2e9b99b703768dded69d99c575f307f
joshuavottero <joshuavottero@outlook.com>
2022-02-12 18:21:47 +0000

    added components and file Login

---
+++ client/src/components/Login.js
@@ -0,0 +1,21 @@
+export default function Appointment(props) {
+
+
+  return (
+    <div class="login">
+      <h3> Login </h3>
+      <form>
+        <div>
+          <label>UserName</label>
+          <input placeholder="enter UserName here"/>
+        </div>
+        <div>
+        <label>Password</label>
+          <input placeholder="enter Password here"/>
+        </div>
+        <button>Submit</button>
+      </form>     
+    </div>
+ 
+  );
+}
