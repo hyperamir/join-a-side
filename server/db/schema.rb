21233cdfb5613aa8812528579cd2af7ca071dcfa
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-02-12 12:07:51 -0500

    change password to password_digest in users

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_12_150402) do
+ActiveRecord::Schema.define(version: 2022_02_12_170605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,10 +49,10 @@ ActiveRecord::Schema.define(version: 2022_02_12_150402) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
     t.string "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "questions"
