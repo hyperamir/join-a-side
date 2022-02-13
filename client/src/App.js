6165fcdc22bd530e522dfbefbb04b227b232885a
Andy Junhyuk Park <pos03142ap@gmail.com>
2022-02-13 03:11:26 -0800

    setup Fontawesome && Sass && add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import './App.scss';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world
-    </h1>
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
   );
 }
 
