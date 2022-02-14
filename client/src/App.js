57f74e3b24b053af23bc7986688c78e94bb2d383
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-02-14 14:42:09 -0500

    fix CORS issue and get data from back-end

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import Navbar from './components/Navbar';
 import Post from './components/Post';
 import logo from './logo.svg';
-import { useEffect, useState } from'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import Login from './components/Login'
 import Register from './components/Register'
@@ -11,26 +11,36 @@ import LogoutButton from './components/LogoutButton'
 //import firebase from './service/firebase'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import "./service/firebase"
+import axios from 'axios'
 
 
 function App() {
-  
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/categories').then((response) => {
+      console.log('data from back end!!: ')
+      console.log(response)
+    }).catch(e => {
+      console.log('error:', e)
+    })
+  }, [])
+
   // changes user 
   //const [user, setUser] = useState({});
   const [user, setUser] = useState(null);
   const auth = getAuth();
   console.log(auth);
-  useEffect(() => { 
-   onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setUser(user.email) 
-      console.log("sign in")
-    } else {
-      console.log("sign out")
-      setUser(null) 
-    }
-   });
-  }, []) 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user.email)
+        console.log("sign in")
+      } else {
+        console.log("sign out")
+        setUser(null)
+      }
+    });
+  }, [])
 
   console.log(user);
 
