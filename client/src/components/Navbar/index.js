ae5148f554833dc74291fa00aa86d99cc08c61b6
Andy Junhyuk Park <pos03142ap@gmail.com>
2022-02-15 13:01:47 -0800

    fix the comment section && comment out search button

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -43,14 +43,14 @@ export default function Navbar(props) {
       </div>
 
       {/* Search Bar */}
-      <div className="font-sans text-black ml-8">
+      {/* <div className="font-sans text-black ml-8">
         <div className="border rounded-full py-2 px-6 overflow-hidden flex bg-white">
           <input type="text" className="w-60" placeholder="Search..." />
           <button className="flex items-center justify-center px-4 border-l">
             <svg className="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" /></svg>
           </button>
         </div>
-      </div>
+      </div> */}
 
       {/* Mobile hamberger menu */}
       <div className="block lg:hidden">
--- client/src/components/Post/Comment.js
+++ client/src/components/Post/Comment.js
@@ -2,54 +2,13 @@ import "./Comment.scss";
 
 export default function Comment(props) {
   return (
-    <div className="flex flex-row justify-center">
+    <div className="flex justify-center">
 
       {/* Comment section for Answer 1 */}
       <div className="flex items-center justify-center p-2">
         <div className="bg-white shadow-xl border p-8 w-3xl">
           <div className="mb-4">
-            <h1 className="font-semibold text-gray-800">Comments Answer 1</h1>
-          </div>
-          <div className="flex justify-center items-center mb-8">
-            <div className="w-1/5">
-              <img className="w-12 h-12 rounded-full border border-gray-100 shadow-sm" src="https://randomuser.me/api/portraits/men/20.jpg" alt="user image" />
-            </div>
-            <div className="w-4/5">
-              <div>
-                <span className="font-semibold text-gray-800">Ezio Dani</span>
-              </div>
-              <div className="">
-                <a href="" className="text-black-600 mr-2">Comments by the user</a>
-              </div>
-              <div>
-                <a href="" className="text-gray-400">Comment posted ago</a>
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-center items-center">
-            <div className="w-1/5">
-              <img className="w-12 h-12 rounded-full border border-gray-100 shadow-sm" src="https://randomuser.me/api/portraits/women/20.jpg" alt="user image" />
-            </div>
-            <div className="w-4/5">
-              <div>
-                <span className="font-semibold text-gray-800">Bianca Chen</span>
-              </div>
-              <div className="">
-                <a href="" className="text-black-600 mr-2">Comments by the user</a>
-              </div>
-              <div>
-                <a href="" className="text-gray-400">Comment posted ago</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Comment section for Answer 2 */}
-      <div className="flex items-center justify-center">
-        <div className="bg-white shadow-xl border p-8 w-3xl">
-          <div className="mb-4">
-            <h1 className="font-semibold text-gray-800">Comments Answer 2</h1>
+            <h1 className="font-semibold text-gray-800">Comment Section for both Answers 1 and Answer 2</h1>
           </div>
           <div className="flex justify-center items-center mb-8">
             <div className="w-1/5">
