aa4dbe7ab182f8937e5dc0cf5f15ee4b91a91cbd
Andy Junhyuk Park <pos03142ap@gmail.com>
2022-02-16 14:40:54 -0800

    insert populated data into front-end UI

--- client/src/components/QuestionList.js
+++ client/src/components/QuestionList.js
@@ -25,13 +25,38 @@ export default function QuestionList(props) {
         category.map(question => {
           return (
             <Link key={question.id} to="/">
-            <div>
-              <h2>{question.title}</h2>
-              <h3>{question.answer_a} </h3>
-              <h3>{question.answer_b} </h3>
-              <h3>{question.vote_a} </h3>
-              <h3>{question.vote_b} </h3>
-            </div>
+              <div className="flex flex-col bg-white px-8 py-6 max-w-lg mx-auto rounded-lg shadow-xl border">
+                <div className="flex justify-center items-center">
+                  <a className="px-2 py-1 bg-indigo-700 text-sm text-green-100 rounded" href="#">Category {question.id}</a>
+                </div>
+                <div className="mt-4">
+                  <a className="text-lg text-gray-700 font-medium" href="#">{question.title}</a>
+                </div>
+                <div className="flex flex-row justify-between p-8">
+                  <button className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold mx-2 py-2 px-4 rounded-full">
+                    {question.answer_a}
+                  </button>
+                  <button className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold mx-2 py-2 px-4 rounded-full">
+                    {question.answer_b}
+                  </button>
+                </div>
+              </div>
+              
+              <div className="flex items-center justify-center p-4">
+                <div className="bg-white shadow-xl border p-8 w-3/6">
+                
+                {/* Status Bar */}
+                <div className="container">
+                  <div className="votes bar"></div>
+                </div>
+
+                {/* Votes */}
+                <div className="flex flex-row justify-between">
+                  <p>{question.vote_a}</p>
+                  <p>{question.vote_b}</p>
+                </div>
+                </div>
+              </div>
             </Link>
           )
         })
