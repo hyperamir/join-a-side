17a7473f148b07d658e68ff09ccc015d6c6a5827
Joshua Vottero <joshuavottero@Joshuas-iMac.local>
2022-02-17 15:36:07 -0500

    not really done yet with submiting questions yet haveing some post requset issues

--- server/app/controllers/questions_controller.rb
+++ server/app/controllers/questions_controller.rb
@@ -1,13 +1,34 @@
 class QuestionsController < ApplicationController
 
   def index
-    category = Category.find params[:category_id]
+    category = Category.find_by(id: params[:category_id])
     render json: category.questions
   end
 
   def show
     question = Question.find params[:id]
     render json: question
+  end
+
+  def create
+      @question = Question.new(question_params)
+      if @question.save
+        render json: @question
+      else
+        render json: @question.errors
+      end
+  end
+
+  private 
+
+  def question_params
+    params.permit(
+      :title,
+      :answer_a,
+      :answer_b,
+      :category_id,
+      :user_id
+    )
   end
 
 end
